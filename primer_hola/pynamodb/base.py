4b0ad93bd7dd3ea3e51b1e52f02106a036e9d5f4
Marco Torres <marco.iturbide99@hotmail.es>
2023-09-06 20:35:46 -0600

    claves de aws borradas

--- primer_hola/pynamodb/base.py
+++ primer_hola/pynamodb/base.py
@@ -5,11 +5,6 @@ from pynamodb.connection.base import Connection
 
 from model import MiTabla
 
-# Configurar las credenciales y la región de AWS
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAY4LPEZ6OWDBAJNPA'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
-os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'  # Cambia esto a tu región
-
 # Definir el modelo de datos
 
 class Base:
